674312e4a27b578b3faf8ab39385ce6c96f44551
giovani <giovani@localhost>
2011-05-06 02:54:15 -0300

    Configurado para rodar o mysql

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,32 +1,38 @@
+import org.codehaus.groovy.grails.orm.hibernate.cfg.GrailsAnnotationConfiguration
+
 dataSource {
-    pooled = true
-    driverClassName = "org.hsqldb.jdbcDriver"
-    username = "sa"
-    password = ""
+	configClass = GrailsAnnotationConfiguration.class
+	pooled = false
+	driverClassName = "com.mysql.jdbc.Driver"
+	username = "root"
+	password =  "root"
+	dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
 }
+
 hibernate {
-    cache.use_second_level_cache = true
-    cache.use_query_cache = true
-    cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
+	cache.use_second_level_cache=true
+	cache.use_query_cache=true
+	cache.provider_class='net.sf.ehcache.hibernate.EhCacheProvider'
 }
+
 // environment specific settings
 environments {
-    development {
-        dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-            url = "jdbc:hsqldb:mem:devDB"
-        }
-    }
-    test {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:hsqldb:mem:testDb"
-        }
-    }
-    production {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:hsqldb:file:prodDb;shutdown=true"
-        }
-    }
+	development {
+		dataSource {
+			dbCreate = "create-drop" // one of 'create', 'create-drop','update'
+			url = "jdbc:mysql://localhost/CenterCarService"
+		}
+	}
+	test {
+		dataSource {
+			dbCreate = "update"
+			url = "jdbc:mysql://localhost/CenterCarService"
+		}
+	}
+	production {
+		dataSource {
+			dbCreate = "update"
+			url = "jdbc:mysql://localhost/CenterCarService"
+		}
+	}
 }
