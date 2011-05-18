2c033f14af558fe044c24fa5847c38face272cce
giovani <giovani@localhost>
2011-05-17 21:22:07 -0300

    Parte de Cadastro

--- grails-app/domain/centercarservice/cadastro/Cadastravel.groovy
+++ grails-app/domain/centercarservice/cadastro/Cadastravel.groovy
@@ -1,12 +1,16 @@
 package centercarservice.cadastro
 
-import centercarservice.financeiro.Caixa
-
 abstract class Cadastravel {
 	String cpf, nome, telefone, rg
-	Calendar dataDeNascimento
-	Endereco endereco
+	Date dataDeNascimento
+	String endereco
 		
     static constraints = {
+		nome(blank:false)
+		cpf()
+		rg(minLength:7, blank:true)
+		dataDeNascimento(validator: {return it < new Date()})
+		telefone(maxLength:10, minLength:8, blank:true)
+		endereco(maxLength:1000, blank:true)
     }
 }
