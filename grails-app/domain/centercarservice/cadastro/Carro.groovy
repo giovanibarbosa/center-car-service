0acdffcb00969634ca29672978d4fbc43b9aad34
giovani <giovani@localhost>
2011-05-25 03:41:06 -0300

    Commit?o da madruga, n?o testado

--- grails-app/domain/centercarservice/cadastro/Carro.groovy
+++ grails-app/domain/centercarservice/cadastro/Carro.groovy
@@ -1,10 +1,12 @@
 package centercarservice.cadastro
 
+import java.sql.Date;
+
 import centercarservice.financeiro.Servico;
 
 class Carro {
 	String placa, nome, motor, cor, tipoDeCombustivel
-	int ano
+	String ano = new Date().getYear().toString()
 	
 	static belongsTo = [cliente:Cliente]
 	
@@ -12,10 +14,18 @@ class Carro {
 	
     static constraints = {
 		nome(blank:false)
-		ano(min:1900)
+		ano(inList:listaAnos())
 		motor()
 		cor()
 		placa(length:7)
 		tipoDeCombustivel(inList:["Gasolina", "Alcool", "GNV", "Flex"])
+	}
+	
+	def List listaAnos() {
+		def List anos
+		for (int i = 1950; i <= (new java.util.Date().getYear().toString()) ; i++) {
+			anos.add(i)
+		}
+		return anos
 	}
 }
