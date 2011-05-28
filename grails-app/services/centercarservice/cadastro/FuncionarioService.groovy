cd79af6c2d9864710bc19da4eb1368b06a51033e
cassioafonso <cassioafonso@localhost>
2011-05-28 18:15:40 -0300

    - Calculo da comissao salarial implementado

---
+++ grails-app/services/centercarservice/cadastro/FuncionarioService.groovy
@@ -0,0 +1,25 @@
+package centercarservice.cadastro
+
+import java.awt.datatransfer.Transferable;
+import org.springframework.transaction.annotation.Transactional;
+import centercarservice.cadastro.Funcionario;
+import centercarservice.financeiro.Salario
+
+class FuncionarioService {
+
+	@Transactional(readOnly = true)
+	def listSalarios() {
+		Salario.list()
+	}
+
+	@Transactional
+	def LinkedHashMap funcionariosNaoPagos() {		
+		Set funcionariosInstanceList = new HashSet()
+		for(Salario s : listSalarios()) {
+			if (s.getPago() == "Nao") {
+				funcionariosInstanceList.add s.getFuncionario()
+			}
+		}
+		return [funcionariosInstanceList  : funcionariosInstanceList , funcionariosInstanceListTotal: funcionariosInstanceList.size()]
+	}
+}
