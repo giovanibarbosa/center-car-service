4228f977f676a90c441099d6e9ff03c2c99b1880
giovani <giovani@localhost>
2011-06-07 20:35:33 -0300

    Rodando com quartz

--- grails-app/domain/centercarservice/cadastro/Funcionario.groovy
+++ grails-app/domain/centercarservice/cadastro/Funcionario.groovy
@@ -1,18 +1,14 @@
 package centercarservice.cadastro
 
-import java.math.BigDecimal;
-
 import centercarservice.financeiro.Salario;
 import centercarservice.financeiro.Venda;
 
 class Funcionario extends Cadastravel {
 	Date dataDeIngresso
-	
+
 	static hasMany = [salarios:Salario, vendas:Venda]
 
-	static constraints = {
-		dataDeIngresso()
-	}
+	static constraints = { dataDeIngresso() }
 
 	String toString() {
 		return "${nome} : ${rg}"
@@ -20,7 +16,7 @@ class Funcionario extends Cadastravel {
 
 	def List getVendasPorPeriodo(Salario s) {
 		def todasVendas = getVendas()
-		def List vendasNoPeriodo = new ArrayList() 
+		def List vendasNoPeriodo = new ArrayList()
 		Date fimDePeriodo = s.getDataDePagamento()
 		Date comecoDePeriodo = fimDePeriodo.clone()
 		comecoDePeriodo.setMonth(fimDePeriodo.getMonth() - 1)
@@ -29,7 +25,7 @@ class Funcionario extends Cadastravel {
 				vendasNoPeriodo.add(venda)
 			}
 		}
-		return vendasNoPeriodo		
+		return vendasNoPeriodo
 	}
 
 	def Integer calculaSalariosAtrasados() {
@@ -45,7 +41,7 @@ class Funcionario extends Cadastravel {
 		return numeroDeSalariosAtrasados;
 	}
 
-		def BigDecimal calculaValorTotalAtrasado() {
+	def BigDecimal calculaValorTotalAtrasado() {
 		BigDecimal valorTotal = 0
 		def salarios = Salario.list()
 		for (Salario s : salarios) {
